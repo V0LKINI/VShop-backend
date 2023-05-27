dd30830b2f7f1548479818688ff89dc99b1902d4
V0LKINI <k.volkov.n@gmail.com>
2023-05-27 11:53:33 +0700

    Added README installation guide

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function (Request $request) {
-    return 'it works';
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function (Request $request) {
+    return 'VShop API startpoint';
 });
